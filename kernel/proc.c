a2bd95b579cf343e52c14841615805f398223f82
D城兔Maziyo <247948681@qq.com>
2022-09-02 11:46:02 +0800

    feat: pgtbl

--- kernel/proc.c
+++ kernel/proc.c
@@ -276,8 +276,7 @@ fork(void)
   np->sz = p->sz;
 
   np->parent = p;
- 
-  np->trace_mask = p->trace_mask;
+
   // copy saved user registers.
   *(np->trapframe) = *(p->trapframe);
 
@@ -484,10 +483,14 @@ scheduler(void)
       }
       release(&p->lock);
     }
+#if !defined (LAB_FS)
     if(found == 0) {
       intr_on();
       asm volatile("wfi");
     }
+#else
+    ;
+#endif
   }
 }
 
